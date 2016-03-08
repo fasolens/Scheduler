9dadeb549e5a9022b7ccaac39f35a30a0561529c
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-03-08 11:06:51 +0100

    fix error code

--- files/usr/bin/container-deploy.sh
+++ files/usr/bin/container-deploy.sh
@@ -5,6 +5,7 @@ SCHEDID=$1
 CONTAINER_URL=$2
 
 ERROR_CONTAINER_NOT_FOUND=100
+ERROR_INSUFFICIENT_DISK_SPACE=101
 
 # TODO: Check if we have sufficient resources to deploy this container.
 # If not, return an error code to delay deployment.
@@ -14,13 +15,11 @@ DISKQUOTA=10000 #K
 DISKSPACE=$(df / --output=avail|tail -n1)
 if (( "$DISKSPACE" < $(( 2000000 + $DISKQUOTA )) )); then
     logger -t container-deploy not enough disk space to deploy container $1;
-    exit 1;
+    exit ERROR_INSUFFICIENT_DISK_SPACE;
 fi
 
 EXISTED=$(docker images -q $CONTAINER_URL)
-
 docker pull $CONTAINER_URL || exit $ERROR_CONTAINER_NOT_FOUND
-
 #retag container image with scheduling id
 docker tag $CONTAINER_URL monroe-$SCHEDID
 if [ -z "$EXISTED" ]; then
