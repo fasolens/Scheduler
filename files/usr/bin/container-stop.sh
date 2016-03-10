154893327e78159ccb6ed2a20932a877b5758d96
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-03-10 09:38:43 +0100

    less output in the cleanup process

--- files/usr/bin/container-stop.sh
+++ files/usr/bin/container-stop.sh
@@ -2,33 +2,32 @@
 #This script should always be run, as long as the container is deployed.
 
 SCHEDID=$1
+STATUS=$2
 CONTAINER=monroe-$SCHEDID
 
-# undo startup and deployment steps
-MNS="ip netns exec monroe"
-
-INTERFACES="usb0 usb1 usb2 wlan0 eth0";
-for IF in $INTERFACES; do
-  if [ -z "$($MNS ip link|grep $IF)" ]; then continue; fi
-  $MNS ip link delete $IF;
-done
-
-ip netns delete monroe
-
-# Stop the container if it is running.
 CID=$( docker ps | grep $CONTAINER | awk '{print $1}' )
 if [ -z "$CID" ]; then
   echo "Container is no longer running.";
 else
+  MNS="ip netns exec monroe";
+
+  INTERFACES="usb0 usb1 usb2 wlan0 eth0";
+  for IF in $INTERFACES; do
+    if [ -z "$($MNS ip link|grep $IF)" ]; then continue; fi
+    $MNS ip link delete $IF;
+  done
+
+  ip netns delete monroe;
+
+  # Stop the container if it is running.
   docker stop --time=10 $CID;
 fi
 
-echo 'finished' > /outdir/$SCHEDID.status
-
-# TODO sync outdir
-
-# when in production, we will also want to delete the expired container image
-# but only if no scheduled experiments are expected to use the same image
+if [ -z "$STATUS" ]; then
+  echo 'finished' > /outdir/$SCHEDID.status;
+else 
+  echo $STATUS > /outdir/$SCHEDID.status;
+fi
 
 REF=$( docker images | grep $CONTAINER | awk '{print $3}' )
 if [ -z "$REF" ]; then
@@ -42,8 +41,10 @@ docker rm $(docker ps -aq) 2>/dev/null
 # clean any untagged containers without dependencies (unused layers)
 docker rmi $(docker images -a|grep '^<none>'|awk "{print \$3}") 2>/dev/null
 
-umount /outdir/$SCHEDID
-rmdir  /outdir/$SCHEDID
-rm     /outdir/${SCHEDID}.disk
-rm     /outdir/${SCHEDID}.counter
+# TODO sync outdir
+
+umount /outdir/$SCHEDID            2>/dev/null  || echo 'Directory is no longer mounted.'
+rmdir  /outdir/$SCHEDID            2>/dev/null   
+rm     /outdir/${SCHEDID}.disk     2>/dev/null 
+rm     /outdir/${SCHEDID}.counter  2>/dev/null
 
