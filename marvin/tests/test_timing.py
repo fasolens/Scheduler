776bc8dd0e9180e22854fb78023bad7d9bc96d5d
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-04-01 09:33:58 +0200

    more intensive timing tests

--- marvin/tests/test_timing.py
+++ marvin/tests/test_timing.py
@@ -32,30 +32,41 @@ class TimingTestCase(unittest.TestCase):
             self.assertIsNotNone(userid)
 
             c = self.sch.db().cursor()
-            c.execute("INSERT OR IGNORE INTO nodes VALUES (?, ?, ?, ?)",
-                      ('1', 'test-node', 'active', 0))
-            self.sch.db().commit()
-            self.sch.set_node_types(1, 'test')
+            nodes = self.sch.get_nodes()
+            if nodes is None or len(nodes) < 25:
+                print "creating 25 nodes...",
+                for nodeid in xrange(1,25):
+                    c.execute("INSERT OR IGNORE INTO nodes VALUES (?, ?, ?, ?)",
+                              (nodeid, 'test %i' % nodeid, 'active', 0))
+                    self.sch.db().commit()
+                    self.sch.set_node_types(nodeid, 'test')
+                print "done."
 
     def test_01_lots_of_recurring(self):
         userid = self.sch.get_users()[0]['id']
         now = time.time()
         until = self.sch.get_scheduling_period()[1]
-        r = self.sch.allocate(userid, 'test', now + 500, 60, 1, 'test', '...',
-                              {'recurrence': 'simple',
-                               'period': 3600,
-                               'until': until})
-        self.assertEqual(r[2]['nodecount'], 1)
-        self.assertEqual(r[2]['intervals'], 744)
-        r = self.sch.allocate(userid, 'test', now + 750, 60, 1, 'test', '...',
-                              {'recurrence': 'simple',
-                               'period': 3600,
-                               'until': until})
-        self.assertEqual(r[2]['nodecount'], 1)
-        self.assertEqual(r[2]['intervals'], 744)
-        after = time.time()
+        print
+        print "creating 25x2 recurring experiments ..."
+        for nodeid in xrange(1,25):
+            before = time.time()
+            r = self.sch.allocate(userid, 'test', now + 500, 60, 1,
+                                  'test', '...',
+                                  {'recurrence': 'simple',
+                                   'period': 3600,
+                                   'until': until})
+            self.assertEqual(r[2]['nodecount'], 1)
+            self.assertEqual(r[2]['intervals'], 744)
+            r = self.sch.allocate(userid, 'test', now + 750, 60, 1, 'test', '...',
+                                  {'recurrence': 'simple',
+                                   'period': 3600,
+                                   'until': until})
+            self.assertEqual(r[2]['nodecount'], 1)
+            self.assertEqual(r[2]['intervals'], 744)
+            after = time.time()
+            print nodeid * 2, after-before, "seconds."
+            self.assertLess(after-before, 10)  # completes in 10 seconds or less
         print "completed in %f seconds." % (after-now)
-        self.assertLess(after-now, 3)  # completes in 3 seconds or less
 
     def test_02_find_slot(self):
         before = time.time()
@@ -70,7 +81,7 @@ class TimingTestCase(unittest.TestCase):
         r = self.sch.find_slot(1, duration=500, nodes=['500', '600'])
         self.assertIsNone(r[0])
         after = time.time()
-        print "completed in %f seconds." % (after-before)
+        print "completed in %f seconds - " % (after-before),
         self.assertLess(after-before, 3)  # completes in 3 seconds or less
 
     def test_99_cleanup(self):
