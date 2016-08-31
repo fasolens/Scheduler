4dbea4e74856040a612ae5903902a6b3dbdf833c
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-08-31 09:30:09 +0200

    modify query, label

--- marvin/scheduler.py
+++ marvin/scheduler.py
@@ -361,8 +361,8 @@ CREATE INDEX IF NOT EXISTS k_times      ON quota_journal(timestamp);
         tasks["future"]=c.fetchone()[0]
         c.execute("SELECT count(*) as count FROM schedule WHERE start < ? AND stop > ?", (now,now))
         tasks["current"]=c.fetchone()[0]
-        c.execute("SELECT count(*) as count FROM schedule s, experiments e WHERE s.expid=e.id AND s.stop > ? GROUP BY e.ownerid", (now,))
-        tasks["distinct users"]=c.fetchone()[0]
+        c.execute("SELECT DISTINCT e.ownerid FROM schedule s, experiments e WHERE s.expid=e.id AND s.stop > ? GROUP BY e.ownerid", (now,))
+        tasks["distinct active users"]=len(c.fetchone())
         activity["schedules"]=tasks
         return activity
 
